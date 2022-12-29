a67eb4cdc18954fec2995a76380c3bed6e00187f
zeynepabali <abali.zeynep@gmail.com>
2022-12-29 22:29:52 +0300

    changing proteins table data

--- drip.py
+++ drip.py
@@ -7,20 +7,20 @@ from datetime import datetime
 from st_aggrid import AgGrid, GridOptionsBuilder, JsCode
 
 st.set_page_config(layout="wide")
-st.title("Interface Dataset")
+st.title("PDB Entries")
 
 proteins_df = pd.read_csv('data/proteins_table.csv')
 interfaces_df = pd.read_csv('data/interfaces_table.csv')
 ligands_df = pd.read_csv('data/ligands_table.csv')
 
-int_builder = GridOptionsBuilder.from_dataframe(proteins_df)
+int_builder = GridOptionsBuilder.from_dataframe(proteins_df[["pdbID", "fda_approved", "num_interfaces", "num_ligands", "interface_str", "ligand_str"]])
 int_builder.configure_default_column(editable=False, filterable=False, cellStyle={'text-align': 'center'})
-int_builder.configure_column("pdbID", header_name="Interface ID", editable=False, )
-int_builder.configure_column("interface_id", header_name="Chain 1")
-int_builder.configure_column("ligands", header_name="Chain 2")
-int_builder.configure_column('fda_approved', header_name="# Chain 1")
-int_builder.configure_column("num_interfaces", header_name="# Chain 2")
-int_builder.configure_column("num_ligands", header_name="# Interface Residues")
+int_builder.configure_column("pdbID", header_name="PDB ID", editable=False, )
+int_builder.configure_column("interface_str", header_name="Interfaces")
+int_builder.configure_column("ligand_str", header_name="Ligands")
+int_builder.configure_column('num_interfaces', header_name="# of Interfaces")
+int_builder.configure_column("num_ligands", header_name="# of Ligands")
+int_builder.configure_column("fda_approved", header_name="FDA Approved")
 int_builder.configure_pagination(enabled=True, paginationAutoPageSize=False, paginationPageSize=20)
 int_builder.configure_selection('single', use_checkbox=True)
 
